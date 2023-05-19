b52e20118e998bd9912bd59ab9057da43e01e08d
GuilhermeSanchesRossi <guilherme.s.rossi@hotmail.com>
2023-05-19 12:39:59 -0300

    primeiro microsserviço: login

---
+++ client/src/App.js
@@ -0,0 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import Login from './Login';
+
+function App() {
+  return <Login />
+}
+
+export default App;
---
+++ client/src/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.css'
+
+const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=044260f338044707bebed72d5669ac67&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+//URL da API do Spotify
+
+export default function Login() {
+    return (
+        <div>
+            <a className='btn btn-success btn-lg' href={AUTH_URL}> Entre com Spotify </a>
+        </div>
+    )
+}
