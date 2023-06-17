96abb12b63d18856806ab95ca11e14bb6b4969a9
GuilhermeSanchesRossi <guilherme.s.rossi@hotmail.com>
2023-06-17 19:57:41 -0300

    criação do barramento, teste de comunicação com microsserviço usuarios

--- usuarios/src/index.js
+++ usuarios/src/index.js
@@ -11,16 +11,28 @@ app.get('/usuarios', (req, res) => {
     res.send(bdUsuarios)
 }); 
 
-app.post('/usuarios', (req, res) => {
+app.post('/usuarios', async (req, res) => {
     jsonSpotify = req.body.spotify_data
     console.log(jsonSpotify)
     bdUsuarios[jsonSpotify.id] = {
         username:jsonSpotify.email, nome:jsonSpotify.display_name
     }   
 
+    await axios.post('http://localhost:10000/eventos', {
+        tipo:'usuarioLogado',
+        dados:{
+            jsonSpotify
+        }
+    })
+
     res.status(201).send(bdUsuarios[jsonSpotify.id]);
 });
 
+app.post("/eventos", (req, res) => {
+    console.log(req.body);
+    res.status(200).send({ msg: "ok" });
+    });
+
 app.listen(5001, (() => {
-    console.log('Lembretes. Porta 5001');
+    console.log('Usuários. Porta 5001');
     }));
