d9e15a4a196cb8c7b0d1c94d57f014818204b15c
Fernando Henriques Neto <nandohneto@gmail.com>
2023-06-30 22:28:49 -0300

    carrosel responsivo

--- frontend/src/components/cartaoAlbum/cartaoAlbum.jsx
+++ frontend/src/components/cartaoAlbum/cartaoAlbum.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './cartaoAlbum.css'
 
-export default function CartaoAlbum({fotoAlbum, nomeMusica, artista, ouvinte}){
-    return (
-        <div className="ctn">
-            <img src={fotoAlbum}/>
+export default function CartaoAlbum({ fotoAlbum, nomeMusica, artista, ouvinte }) {
+   return (
+      <div className='ctnMain text-truncate' style={{ width: '92%' }}>
+         <img src={fotoAlbum} />
             <p className='musica text-truncate'>{nomeMusica}</p>
             <p className='artista'>{artista}</p>
             {ouvinte && <p>{`Ouvido por ${ouvinte}`}</p>}
-        </div>
-  );
+      </div>
+   );
 }
--- frontend/src/components/login/login.jsx
+++ frontend/src/components/login/login.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-import styles from './login.module.css'; // Importe o arquivo CSS aqui
+import './login.css'; // Importe o arquivo CSS aqui
 
 import CarrosselAlbum from '../carrosselAlbum/carrosselAlbum';
 import Spinner from '../spinner/spinner';
@@ -10,7 +10,7 @@ import SpotifyDataProcessor from '../../services/dataProcessor';
 
 export default function Login({ EmAlta, EmAltaBR, Comunidades}) {
    const AUTH_URL = 'http://localhost:8888/login'
-   const titulosPrincipais = {fontSize: 30};
+   const titulosPrincipais = {};
 
    // SliderSettings breakpoints: window Size: 1024px, slidesToShow: 2
    const breakpointsAlbuns = [[1200, 3], [992, 2], [768, 1] ];
@@ -38,7 +38,7 @@ export default function Login({ EmAlta, EmAltaBR, Comunidades}) {
    },[]);
 
    return(
-         <div className={`container-md  ${styles.login}`}>
+         <div className={`container-md login`}>
                <div className='row'>
                   <div className='col-md-10 col-6'>
                      <div className='row'>
@@ -49,17 +49,16 @@ export default function Login({ EmAlta, EmAltaBR, Comunidades}) {
                               <a href={AUTH_URL}>Entre ou cadastra-se</a>
                         </div>
                      </div>
-                     <div className='row my-3' style={titulosPrincipais}>
+                     <div className='row my-3 mainHedaers'>
                         <p>Em Alta na Gringa</p>
                         {dataAlbuns.length === 0 ? <Spinner/> : <CarrosselAlbum listaAlbum={dataAlbuns} listaBreakpoints={breakpointsAlbuns}/>}
                      </div>
-                     <div className='row my-3' style={titulosPrincipais}>
+                     <div className='row my-3 mainHedaers'>
                         <p>Em alta no BR</p>
                         {dataAlbuns.length === 0 ? <Spinner/> : <CarrosselAlbum listaAlbum={dataAlbuns} listaBreakpoints={breakpointsAlbuns}/>}
                      </div>
                   </div>
-                  {/* <div className='col-xl-1 col-lg-0'></div> */}
-                  <div className='col-md-2 col-6 mx-auto text-center' style={titulosPrincipais}>
+                  <div className='col-md-2 col-6 mx-auto text-center mainHedaers'>
                      <p>Artistas em Alta</p>
                      <ScrollArtist/>
                   </div>
