7bd05296afd761951ecbe36aa2c5a975cd02c390
GuilhermeSanchesRossi <guilherme.s.rossi@hotmail.com>
2023-08-19 22:14:12 -0300

    ajuste no micros. usuarios + barramento de eventos

--- usuarios/src/index.js
+++ usuarios/src/index.js
@@ -11,16 +11,26 @@ app.get('/usuarios', (req, res) => {
     res.send(bdUsuarios)
 }); 
 
-app.post('/usuarios', (req, res) => {
+app.post('/usuarios', async (req, res) => {
     jsonSpotify = req.body.spotify_data
     console.log(jsonSpotify)
     bdUsuarios[jsonSpotify.id] = {
-        username:jsonSpotify.email, nome:jsonSpotify.display_name
-    }   
+        nome:jsonSpotify.display_name, fotoPerfil:jsonSpotify.images[0].url, nac:jsonSpotify.country,
+        follows:jsonSpotify.followers.total
+    }  
+    console.log(bdUsuarios[jsonSpotify.id].nome, bdUsuarios[jsonSpotify.id].fotoPerfil, 
+        bdUsuarios[jsonSpotify.id].nac, 
+        bdUsuarios[jsonSpotify.id].follows) 
 
+    await axios.post('http://localhost:10000/eventos', {
+        tipo:'usuarioLogado',
+        dados:{
+            jsonSpotify
+        }
+    })
     res.status(201).send(bdUsuarios[jsonSpotify.id]);
 });
 
 app.listen(5001, (() => {
-    console.log('Lembretes. Porta 5001');
+    console.log('Users. Porta 5001');
     }));
