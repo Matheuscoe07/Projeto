69001ea0c2d8f3414ccfbedbcc56137b13a8ba63
GuilhermeSanchesRossi <guilherme.s.rossi@hotmail.com>
2023-09-06 12:06:01 -0300

    posts para modelo MVCS, comentario e curtida fora

--- posts/src/index.js
+++ posts/src/index.js
@@ -1,27 +1,16 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const axios = require("axios");
-const { v4: uuidv4 } = require('uuid');
+import express from "express";
+import bodyParser from "body-parser";
+import { postsController, router } from './postsController.js';
+import ENUM from '../../Util/src/enums.js';
 
 const app = express();
+
 app.use(bodyParser.json());
 
-const bdPostsDosUsuarios = {};
+app.use('/posts', router);
 
-app.get('/usuarios/:username/posts', (req, res) => {
-    res.send(bdPostsDosUsuarios[[req.params.username]] || [] )
-}); 
-
-app.put('/usuarios/:username/posts', (req, res) => {
-    const idPost = uuidv4();
-    const { titulo, imagem } = req.body;
-    const postsDoUsuario = bdPostsDosUsuarios[req.params.username] || [];
-    postsDoUsuario.push({ [idPost]: { titulo, imagem } });
-    bdPostsDosUsuarios[req.params.username] = postsDoUsuario;
-
-    res.status(201).send(postsDoUsuario);
+app.listen(ENUM.portas.PORTA_POSTS, () => {
+  console.log(`Posts: Porta ${ENUM.portas.PORTA_POSTS}`);
 });
 
-app.listen(5002, (() => {
-    console.log('Lembretes. Porta 5001');
-    }));
+
